29cb47661f8e05df18004845c6fd9fb0204614eb
wusiyu <stephenwu2019@163.com>
2019-11-20 10:07:08 +0800

    don't set baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -10,7 +10,7 @@ import axios from "axios";
 
 let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  baseURL: 'http://localhost:12331'
+  //baseURL: 'http://localhost:12331'
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
